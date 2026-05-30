b835bcdd28adc36305b76cef48bb15e6e695e191
enghillner-glitch <enghillner-glitch@users.noreply.github.com>
2026-05-29 21:29:13 -0300

    Conectar site ao Supabase

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://lzuaqhmjzwchkesxnocz.supabase.co",
+  supabaseAnonKey: "sb_publishable_pobjUqA7wrT8fECkDOrdRQ_RjjLsVIH",
   productsTable: "products",
   storageBucket: "product-images"
 };
